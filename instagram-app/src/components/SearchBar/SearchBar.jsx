25e75f7b343e5a5b1c64f668ae0d6d990d1e0eb0
EmilyAbrahart <emilyabrahart@gmail.com>
2019-06-04 16:14:40 +0100

    Added Instagram img text

--- instagram-app/src/components/SearchBar/SearchBar.jsx
+++ instagram-app/src/components/SearchBar/SearchBar.jsx
@@ -5,19 +5,25 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faCompass } from '@fortawesome/free-regular-svg-icons';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 
 const igLogo = <FontAwesomeIcon icon={faInstagram} />;
 const profileIcon = <FontAwesomeIcon icon={faUser} />;
 const heartIcon = <FontAwesomeIcon icon={faHeart} />;
 const discoverIcon = <FontAwesomeIcon icon={faCompass} />;
-const searchIcon = <FontAwesomeIcon icon={faSearch} />;
 
 
 const SearchBar = () => {
 	return (
 		<nav>
-			<div className="nameLogo">{igLogo} | Instagram </div>
+			<div className="nameLogo">
+				<div className="logo">{igLogo}</div> <div>|</div>
+				<div className="logoText">
+					<img
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/640px-Instagram_logo.svg.png"
+					/>
+				</div>
+			</div>
 			<input className="searchInput" type="text" placeholder="Search" />
 			<div className="navIcons">
 				<div>{discoverIcon}</div> <div>{heartIcon}</div>
