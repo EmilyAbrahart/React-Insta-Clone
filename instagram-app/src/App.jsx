69c35bb41485b7d514deb1dcb7ff1c8b2a5cb2de
EmilyAbrahart <emilyabrahart@gmail.com>
2019-06-05 14:36:16 +0100

    Create PostsPage component and render through App

--- instagram-app/src/App.jsx
+++ instagram-app/src/App.jsx
@@ -1,47 +1,16 @@
 import React from 'react';
-import dummyData from './dummy-data';
-import SearchBar from './components/SearchBar/SearchBar';
-import PostContainer from './components/PostContainer/PostContainer';
+import PostPage from './components/PostContainer/PostsPage'
 import './App.css';
 
 class App extends React.Component {
 	constructor(props) {
-		super(props);
-		this.state = {
-			postData: [],
-			searchQuery: '',
-			searchData: []
-		};
-	}
-	componentDidMount() {
-		this.setState({
-			postData: dummyData
-		});
-	}
-	searchChangeHandler = event => {
-		this.setState({ searchQuery: event.target.value });
-		this.searchPosts();
-	};
-
-	searchPosts = () => {
-		this.setState(state => {
-			// if (state.searchQuery.length !== 0)
-			state.postData.filter(post => {
-				post.username.toLowerCase().includes(state.searchQuery);
-				})
-				// else if (state.searchQuery.length === 0) {state.postData= dummyData }
-			})}
-			 
-			
-
-	render() {
-		return (
-			<div className="App">
-				<SearchBar searchQuery={this.state.searchQuery} searchChangeHandler={this.searchChangeHandler}/>
-				<PostContainer postContent={this.state.postData} />
-			</div>
-		);
-	}
+		super(props);	
 }
 
+render (){
+	return (
+		<PostPage />
+	)
+}
+}
 export default App;
