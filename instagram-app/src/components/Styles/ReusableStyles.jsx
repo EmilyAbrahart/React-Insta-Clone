3d19cc80d3b4b21ff13ff201db48336c7570ab4e
EmilyAbrahart <emilyabrahart@gmail.com>
2019-06-07 00:57:38 +0100

    Comment Section and Header styling, additional reusable styles. CSS files removed.

--- instagram-app/src/components/Styles/ReusableStyles.jsx
+++ instagram-app/src/components/Styles/ReusableStyles.jsx
@@ -11,9 +11,14 @@ export const FlexFunc = (direction, justifyC, alignI) => {
 };
 
 export const ContainerDiv = styled.div`
-	${FlexFunc(
-		`${props => props.direction}`,
-		`${props => props.justify}`,
-		`${props => props.align}`
-	)}
+	${FlexFunc('column', 'center', 'center')};
+	width: 100%;
+`;
+
+export const Input = styled.input`
+	text-align: center;
+	border: 1px solid rgba(211, 211, 211, 0.8);
+	border-radius: 0.5rem;
+	width: ${props => props.inputWidth || '70%'};
+	height: 1.5rem;
 `;
